5deef4c6838588338d1f19706a01bc485b7aae8d
59763227 <59763227@qq.com>
2016-05-21 22:18:30 +0800

    add

--- road-accidents-data/src/main/java/com/gl/roadaccidents/repository/RoadAccidentRepository.java
+++ road-accidents-data/src/main/java/com/gl/roadaccidents/repository/RoadAccidentRepository.java
@@ -1,6 +1,7 @@
 package com.gl.roadaccidents.repository;
 
 import com.gl.roadaccidents.model.RoadAccident;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
@@ -9,6 +10,6 @@ import javax.persistence.NamedNativeQuery;
 /**
  * Created by gavin on 16-5-13.
  */
-public interface RoadAccidentRepository extends CrudRepository<RoadAccident,Long>{
+public interface RoadAccidentRepository extends JpaRepository<RoadAccident,Long> {
 
 }
---
+++ road-accidents-data/src/main/java/com/gl/roadaccidents/service/RoadAccidentDataQueryService.java
@@ -0,0 +1,8 @@
+package com.gl.roadaccidents.service;
+
+/**
+ * Created by gavin on 16-5-21.
+ */
+public interface RoadAccidentDataQueryService {
+
+}
